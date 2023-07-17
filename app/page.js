7c7eddaa668889ec44b9d859d135b809078245a7
YTorresL <yalithtorres@gmail.com>
2023-07-17 13:24:28 -0400

    Error

--- app/page.js
+++ app/page.js
@@ -7,36 +7,8 @@ import "slick-carousel/slick/slick-theme.css"
 import Parrafo from "@/components/Paragraph/page"
 import Titulo from "@/components/Title/page"
 import ImageWidth from "@/components/ImageWidth/page"
-import Link from "next/link"
 import OpacityAnimation from "@/components/OpacityAnimation/page"
-import { Instagram, TikTok, Youtube, Arrow } from "@/components/Icons/page"
-
-const sliderPrincipal = [
-  {
-    image: "/1.jpg",
-  },
-  {
-    image: "/2.jpg",
-  },
-  {
-    image: "/3.jpg",
-  },
-  {
-    image: "/4.jpg",
-  },
-  {
-    image: "/5.jpg",
-  },
-  {
-    image: "/6.jpg",
-  },
-  {
-    image: "/7.jpg",
-  },
-  {
-    image: "/8.jpg",
-  },
-]
+import { Arrow } from "@/components/Icons/page"
 
 const proceso = [
   {
@@ -58,26 +30,6 @@ const proceso = [
   {
     title: "Inicia tu carrera",
     description: `Estudia en las mejores universidades de Rusia y alcanza tus metas.`,
-  },
-]
-
-const link = [
-  {
-    name: "Instagram",
-    link: "https://www.instagram.com/zdorovo.russia/",
-    svg: <Instagram className="w-11 h-11" />,
-  },
-  {
-    name: "Youtube",
-    link: "https://www.youtube.com/@zdorovo.russia",
-    svg: <Youtube className="w-11 h-11 -ml-10 sm:ml-1" fill="#fff" />,
-  },
-  {
-    name: "Tik Tok",
-    link: "https://www.tiktok.com/@zdorovo.russia?is_from_webapp=1&sender_device=pc",
-    svg: (
-      <TikTok className="w-[3.5rem] h-[3.5rem] -mt-[0.45rem] -ml-[5.2rem] sm:ml-0" />
-    ),
   },
 ]
 
@@ -149,16 +101,6 @@ export default function Home() {
     </div>
   )
 
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 3000,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 5000,
-  }
-
   const settings3 = {
     dots: true,
     infinite: true,
@@ -191,18 +133,22 @@ export default function Home() {
     <>
       <main>
         <div className="w-[85%] h-[35rem] mx-auto">
-                <div className="flex flex-col justify-center h-full lg:w-[80%] md:w-[70%] w-[90%] ml-[7%]">
-                  <h1 className="font-bold text-white text-2xl lg:text-6xl sm:text-4xl my-0">
-                    DILE <span className="text-[#FFBD59]">HOLA A RUSIA</span>
-                  </h1>
-                  <Parrafo styles="sm:text-4xl text-xl">
-                    Sin importar si tienes un conocimiento mínimo del idioma y
-                    obtén tu <span className="text-[#FFBD59]">visado</span> en <span className="text-[#FFBD59]">menos de 90 días</span>
-                  </Parrafo>
-                </div>
+          <div className="flex flex-col justify-center h-full lg:w-[80%] md:w-[70%] w-[90%] ml-[7%]">
+            <h1 className="font-bold text-white text-2xl lg:text-6xl sm:text-4xl my-0">
+              DILE <span className="text-[#FFBD59]">HOLA A RUSIA</span>
+            </h1>
+            <Parrafo styles="sm:text-4xl text-xl">
+              Sin importar si tienes un conocimiento mínimo del idioma y obtén
+              tu <span className="text-[#FFBD59]">visado</span> en{" "}
+              <span className="text-[#FFBD59]">menos de 90 días</span>
+            </Parrafo>
+          </div>
         </div>
         <div className="flex flex-col justify-center items-center">
-          <div className="mb-8"><Arrow className="w-16 h-16" fill="#fff"/></div></div>
+          <div className="mb-8">
+            <Arrow className="w-16 h-16" fill="#fff" />
+          </div>
+        </div>
         <OpacityAnimation>
           <div className="w-[85%] md:w-[50%] mx-auto">
             <YoutubeEmbed embedId="OlI_glpNGUY " />
@@ -212,14 +158,27 @@ export default function Home() {
           <div className="sm:w-[60%] mx-auto py-16 w-[85%]">
             <div>
               <OpacityAnimation>
-                <Titulo styles="text-white">¿<span className="text-[#FFBD59]">IDIOMA</span>? Не волнуйся</Titulo>
+                <Titulo styles="text-white">
+                  ¿<span className="text-[#FFBD59]">IDIOMA</span>? Не волнуйся
+                </Titulo>
                 <Parrafo styles="mt-4 text-xl">
-                  <span className="text-[#FFBD59]">¿No entendiste verdad?</span> Tranquilo yo estaba igual al <span className="text-[#FFBD59]">principio </span>
-                  y ya pase por ese proceso, es <span className="text-[#FFBD59]">mas sencillo</span> de lo que crees.
-                  <span className="text-[#FFBD59]"> Uno de los primeros pasos</span> que realizaremos será en la
-                  inscripción de la facultad de idioma, por cierto, no te
-                  olvides, tenemos la comunidad <span className="text-[#FFBD59]">ZDOROVO</span> en rusia que hara <span className="text-[#FFBD59]">mas
-                  agradable tu experiencia.</span>
+                  <span className="text-[#FFBD59]">¿No entendiste verdad?</span>{" "}
+                  Tranquilo yo estaba igual al{" "}
+                  <span className="text-[#FFBD59]">principio </span>y ya pase
+                  por ese proceso, es{" "}
+                  <span className="text-[#FFBD59]">mas sencillo</span> de lo que
+                  crees.
+                  <span className="text-[#FFBD59]">
+                    {" "}
+                    Uno de los primeros pasos
+                  </span>{" "}
+                  que realizaremos será en la inscripción de la facultad de
+                  idioma, por cierto, no te olvides, tenemos la comunidad{" "}
+                  <span className="text-[#FFBD59]">ZDOROVO</span> en rusia que
+                  hara{" "}
+                  <span className="text-[#FFBD59]">
+                    mas agradable tu experiencia.
+                  </span>
                 </Parrafo>
               </OpacityAnimation>
             </div>
@@ -261,18 +220,23 @@ export default function Home() {
           </OpacityAnimation>
         </div>
         <div>
-        <div className="bg-white py-4">
-        <div className="w-[85%] sm:w-[60%] mx-auto"> <div className="sm:w-[80%]"><Titulo styles="text-[#FFBD59]">
-                      Paso a paso desde que estas en tu país hasta que llegas a
-                      RUSIA
-                    </Titulo></div></div></div>
-        
+          <div className="bg-white py-4">
+            <div className="w-[85%] sm:w-[60%] mx-auto">
+              {" "}
+              <div className="sm:w-[80%]">
+                <Titulo styles="text-[#FFBD59]">
+                  Paso a paso desde que estas en tu país hasta que llegas a
+                  RUSIA
+                </Titulo>
+              </div>
+            </div>
+          </div>
+
           <div className="w-[85%] sm:w-[60%] mx-auto pb-16">
             <div className="flex flex-col items-center justify-center md:flex-row">
               <div className="w-full">
                 <ul className="mt-12">
                   <OpacityAnimation>
-                    
                     {proceso.map((item, index) => (
                       <li className="flex my-3 leading-6" key={index}>
                         <div>
@@ -295,33 +259,32 @@ export default function Home() {
           </div>
         </div>
         <div>
-
-            <div className="w-[85%] mx-auto py-10">
-              <div className="flex flex-col items-center justify-center md:flex-row">
-                <div className="w-full md:w-1/2 md:mr-10 lg:mr-20">
-                  <ImageWidth
-                    src="/estudiante.jpg"
-                    alt="Ellos ya están viviendo la experiencia de pertenecer en la comunidad latina de mas rapido crecimiento en Rusia con ZDOROVO"
-                  />
-                </div>
-                <OpacityAnimation>
-                  <div className="my-6">
-                  <Titulo styles="text-[#FFBD59] uppercase">
-                      Comunidad ZDOROVO
-                    </Titulo>
-                    <p className="text-xl font-bold text-white sm:text-2xl md:text-3xl m-0 mt-3">
-                      Ellos ya están viviendo la experiencia de pertenecer en la
-                      comunidad latina de mas rapido crecimiento en Rusia con
-                      ZDOROVO
-                    </p>
-                    <button className="px-6 py-2 mt-6 text-white text-lg bg-[#FFBD59] font-bold uppercase hover:bg-[#FFBD59] transition duration-500 ease-in-out rounded-lg">
-                      ¿Quieres ser el proximo en Rusia?
-                    </button>
-                  </div>
-                </OpacityAnimation>
+          <div className="w-[85%] mx-auto py-10">
+            <div className="flex flex-col items-center justify-center md:flex-row">
+              <div className="w-full md:w-1/2 md:mr-10 lg:mr-20">
+                <ImageWidth
+                  src="/estudiante.jpg"
+                  alt="Ellos ya están viviendo la experiencia de pertenecer en la comunidad latina de mas rapido crecimiento en Rusia con ZDOROVO"
+                />
               </div>
+              <OpacityAnimation>
+                <div className="my-6">
+                  <Titulo styles="text-[#FFBD59] uppercase">
+                    Comunidad ZDOROVO
+                  </Titulo>
+                  <p className="text-xl font-bold text-white sm:text-2xl md:text-3xl m-0 mt-3">
+                    Ellos ya están viviendo la experiencia de pertenecer en la
+                    comunidad latina de mas rapido crecimiento en Rusia con
+                    ZDOROVO
+                  </p>
+                  <button className="px-6 py-2 mt-6 text-white text-lg bg-[#FFBD59] font-bold uppercase hover:bg-[#FFBD59] transition duration-500 ease-in-out rounded-lg">
+                    ¿Quieres ser el proximo en Rusia?
+                  </button>
+                </div>
+              </OpacityAnimation>
             </div>
           </div>
+        </div>
         <div>
           <div className="w-[85%] mx-auto py-16">
             <div className="flex flex-col items-center justify-center md:flex-row">
@@ -364,27 +327,26 @@ export default function Home() {
               height={800}
               className="w-56 md:w-72 lg:w-80 mb-6"
             />
-    
-              <ul className="text-lg">
-                <li className="mb-2">
-                  <p>
-                    Contáctanos para saber mas información sobre los programas.
-                  </p>
-                </li>
-                <li className="mb-2">
-                  <h2 className="mb-2 text-lg font-bold uppercase">
-                    Numero de contacto
-                  </h2>
-                  <p>+495 95220935</p>
-                </li>
-                <li className="mb-2">
-                  <h2 className="mb-2 text-lg font-bold uppercase">
-                    Correo electronico
-                  </h2>
-                  <p>informacionzdorovo@gmail.com</p>
-                </li>
-              </ul>
 
+            <ul className="text-lg">
+              <li className="mb-2">
+                <p>
+                  Contáctanos para saber mas información sobre los programas.
+                </p>
+              </li>
+              <li className="mb-2">
+                <h2 className="mb-2 text-lg font-bold uppercase">
+                  Numero de contacto
+                </h2>
+                <p>+495 95220935</p>
+              </li>
+              <li className="mb-2">
+                <h2 className="mb-2 text-lg font-bold uppercase">
+                  Correo electronico
+                </h2>
+                <p>informacionzdorovo@gmail.com</p>
+              </li>
+            </ul>
           </div>
         </div>
       </footer>
