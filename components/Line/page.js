cbcefa398294657e8205211504a1e9c1da1f38b4
YTorresL <yalithtorres@gmail.com>
2023-07-18 03:05:11 -0400

    DPage

--- components/Line/page.js
+++
@@ -1,9 +0,0 @@
-export default function Line({ center }) {
-  return (
-    <div
-      className={`h-1 bg-[#6083D4] w-28 my-5 rounded-full ${
-        center && "mx-auto"
-      }`}
-    ></div>
-  )
-}
